013a07a6ae9b710333b3f86e45acf5bd2ee7e5e9
Pichau <rraulhenrique049@gmail.com>
2024-06-07 21:07:14 -0300

    segundo commit

---
+++ Projeto2/Variaveis.py
@@ -0,0 +1,10 @@
+caixadenumeros = "mulher"
+caixadenumeros2 = "felicidade"
+
+caixa = [caixadenumeros,caixadenumeros2]
+print([caixa,caixa,caixa,caixa,caixa])
+
+if(caixadenumeros!=caixadenumeros2):
+    print("uva")
+else:
+    print("morango")
